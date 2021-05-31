3dee7dd81cd70602802c8d3e21648720b035facf
deiday_@naver.com <deiday_@naver.com>
2021-05-31 15:32:28 +0900

    Final01

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Vector3 camOffset = new Vector3(0.0f, 0.0f, -0.01f);
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = target.TransformPoint(camOffset);
+        transform.LookAt(target);
+    }
+}
